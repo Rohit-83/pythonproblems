cb7d38113f222b47e3988efaac54e6f0da80aa63
Rohit Kumar <rk5744081@gmail.com>
2021-04-18 15:08:27 +0530

    use of zip method

---
+++ zip.py
@@ -0,0 +1,8 @@
+#application of zip function
+#if we have to add two arr with their respective elements
+#then we use zip
+a = [1,2,3]
+b = [3,4,5]
+#now we use list comprehension
+value = [x+y for x,y in zip(a,b)]
+print(value)
