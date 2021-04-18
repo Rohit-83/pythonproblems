86655fb67ea49ccd5f8b075acce5ce28ef035b9e
Rohit Kumar <rk5744081@gmail.com>
2021-04-18 15:07:14 +0530

    permutation Backtrack modified

--- prermnutationBacktrack.py
+++ prermnutationBacktrack.py
@@ -3,13 +3,14 @@
 #output--> "ABC","BAC","CAB","ACB","BCA","CBA"
 
 string = "ABC"
+#we convert this into list bcz string object does not supporr changing and assignment
 output = list(string)
 n=len(string)
 l=0
 r=n-1
 def permutation(string,output,l,r):
   if l==r:
-    print("".join(output))
+    print("".join(output),end = " ")
   else:
     for i in range(l,r+1):
       output[l],output[i] = output[i],output[l]
