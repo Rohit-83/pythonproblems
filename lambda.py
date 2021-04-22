90506aef853cc8cd8f6eb40f9c2dc7c4d6fc8200
Rohit Kumar <rk5744081@gmail.com>
2021-04-22 23:34:38 +0530

    modified

--- lambda.py
+++ lambda.py
@@ -4,3 +4,13 @@ print(func(5,6))
 #lambda function using if and else
 func1=lambda i: (i*i if i%2==0 else -i)
 print(func1(101))
+
+
+
+#application of lambda and sort function
+#wap to sort an array according to another array
+cars = ["Ford","Merccitiz","BMW","VW"]
+#we create an array with value 1 to 4
+index = list(range(4))
+index.sort(key = lambda i:cars[i],reverse = True)
+print(index)
