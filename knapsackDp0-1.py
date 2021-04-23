44a9450b23dfc43df102aa33822eb5ac858ac548
Rohit Kumar <rk5744081@gmail.com>
2021-04-23 23:51:08 +0530

    0-1 knapsack dynamic approach

--- knapsackDp0-1.py
+++ knapsackDp0-1.py
@@ -1,8 +1,11 @@
+#wap to solve 0-1 knapsack problem with dynamic approach
+
+
+
 def knapsack(weights,profit,capacity,n):
   if n == 0 or capacity ==0:
     return 0
   if weights[n-1]<= capacity:
-    capacity = capacity-weights[n-1]
     return max(profit[n-1] + knapsack(weights,profit,capacity-weights[n-1],n-1),knapsack(weights,profit,capacity,n-1))
   elif (weights[n-1]>capacity):
     return knapsack(weights,profit,capacity,n-1)
