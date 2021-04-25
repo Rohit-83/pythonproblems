f141937a108fba98e3b3b06c2776127c532477e1
Rohit Kumar <rk5744081@gmail.com>
2021-04-25 23:27:30 +0530

    knapsack dp

--- knapsackDp0-1.py
+++ knapsackDp0-1.py
@@ -52,3 +52,42 @@ def knapsackm(weights,profit,capacity,n,arr):
 
 print(knapsackm(weights,profit,capacity,n,arr))
 
+
+
+
+#now we solve this problem with top down approach
+t = []
+row = 4
+column = 52
+for i in range(row):
+  col = []
+  for j in range(column):
+    col.append(0)
+  t.append(col)
+def knapsacktd(wt,value,W,arr):
+  n =  len(wt)
+  for i in range(1,n+1):
+    for j in range(1,W+1):
+      if i == 0 or j == 0 :
+        t[i][j] = 0
+      elif wt[i-1] <= j:
+        t[i][j] = max(value[i-1]+t[i-1][j-wt[i-1]],t[i-1][j])
+      elif wt[i-1]>j:
+        t[i][j] = t[i-1][j]
+  return t[n][W]
+
+
+wt = [10,20,30]
+value = [60,100,120]
+W = 50
+print(knapsacktd(wt,value,W,arr))
+
+
+
+
+
+
+
+
+
+
