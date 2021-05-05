eb452f43c7ff409dd1452f90c58369da531b14ff
Rohit Kumar <rk5744081@gmail.com>
2021-05-05 20:36:20 +0530

    unbound knapsack use of dP

--- unboundKnapsack.py
+++ unboundKnapsack.py
@@ -18,3 +18,38 @@ for i in index:
     else:
       flag = False 
 print(total)
+
+
+#unbound knapsack 
+#given an wt array  and value array and a knapsack we have to find the maximum profit and W i.e. max weight of knapsack
+#here we can repeat the item i.e we can store the item more than one time 
+
+wt = [1,3,4,5]
+W = 8
+value = [10,40,50,70]
+t = []#creation of matrix 
+n = len(wt)
+for i in range(n+1):
+  col = []
+  for j in range(W+1):
+    col.append(0)
+  t.append(col)
+
+def unbound_knapsack(arr,n,W,value,t):
+  #initialization
+  for i in range(n+1):
+    for j in range(W+1):
+      if i == 0:
+        t[i][j] = 0
+      if j == 0:
+        t[i][j] = 0 
+  for i in range(1,n+1):
+    for j in range(1,W+1):
+      if arr[i-1] <= j:
+        t[i][j] = max(value[i-1]+t[i][j-arr[i-1]],t[i-1][j])
+      else:
+        t[i][j] = t[i-1][j]
+
+  return t[n][W] 
+
+print(unbound_knapsack(wt,n,W,value,t))
