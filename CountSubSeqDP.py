43e61f18b6cf4ff7be796927341b16ab1ac88c0c
Rohit Kumar <rk5744081@gmail.com>
2021-05-12 20:12:19 +0530

    count subsequence

--- CountSubSeqDP.py
+++ CountSubSeqDP.py
@@ -1,5 +1,5 @@
 #count no. of longest common subsest subsequence 
-#with dynamic approach
+#with dynamic approach not top down approach
 arr1 = ["a","b","c","d","g","h"]
 arr2 = ["a","e","d","f","h","r"]
 n = len(arr1)
@@ -13,10 +13,13 @@ for i in range(n+1):
     col.append(-1)
   t.append(col)
 def counting(arr1,arr2,n,m,t):
+  #base case bcz we use recursion here
   if n==0 or m==0:
     return 0
+  #checking if value is present in matrix or not
   if t[n][m] != -1:
-    return t[n][m] 
+    return t[n][m]
+  #if not present then find
   if arr1[n-1] == arr2[m-1]:
     t[n][m] = (1+counting(arr1,arr2,n-1,m-1,t))
     return 1+counting(arr1,arr2,n-1,m-1,t)
