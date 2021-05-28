75c0f452e88af79bc8a52ea666b86345f8bd464f
Rohit Kumar <rk5744081@gmail.com>
2021-05-28 23:59:01 +0530

    matrix chain multiplication recursive and memoization

--- MatrixCMult.py
+++ MatrixCMult.py
@@ -16,3 +16,31 @@ arr=[10,20,30]
 n=len(arr)
 j=n-1
 print(solve(arr,1,n-1))
+
+
+#bottom up approach memoization tabulization (overlapping subsproblem)
+import sys
+def solve1(arr,i,j,t):
+  #base case
+  if i>=j:
+    return 0
+  if t[i][j]!=-1:
+    return t[i][j]
+  mn=sys.maxsize
+  for k in range(i,j):
+    temp=solve1(arr,i,k,t)+solve1(arr,k+1,j,t)+arr[i-1]*arr[k]*arr[j]
+
+    if temp<mn:
+      mn=temp
+  t[i][j] = mn
+  return mn
+
+t=[]
+arr=[10,20,30,40,50,60,70,80,90,100]
+n=len(arr)
+for i in range(100):
+  col=[]
+  for j in range(100):
+    col.append(-1)
+  t.append(col)
+print(solve1(arr,1,n-1,t))
